700bdf9f14de95aef0befba25f37fa5d711e3b91
Ramesh Kumar P <kiruthi.rameshkumar@gmail.com>
2025-07-05 15:28:17 +0530

    Updates

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          ramesh.us
+        </p>
+        <a
+          className="App-link"
+          href="https://ramesh.us"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ramesh.us
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
